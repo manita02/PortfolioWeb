0add29d65c5ec0a107736f3f888b7674ef9f961d
ana.juarez <manitacoqui@gmail.com>
2026-06-24 20:18:27 -0300

    Se crea la interfaz RTipoUbicacion para gestionar operaciones de la entidad TipoUbicacion, incluyendo métodos para buscar por nombre y verificar existencia.

---
+++ back_local/src/main/java/com/portfolioweb/aj/Repositorio/RTipoUbicacion.java
@@ -0,0 +1,14 @@
+package com.portfolioweb.aj.Repositorio;
+
+import com.portfolioweb.aj.Entidad.TipoUbicacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RTipoUbicacion extends JpaRepository<TipoUbicacion, Long> {
+
+    Optional<TipoUbicacion> findByNombre(String nombre);
+
+    boolean existsByNombre(String nombre);
+}
