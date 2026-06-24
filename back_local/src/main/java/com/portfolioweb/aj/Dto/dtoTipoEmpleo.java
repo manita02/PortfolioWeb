339ed16333e10089165112c7358c8da3a2f39a86
ana.juarez <manitacoqui@gmail.com>
2026-06-24 20:16:34 -0300

    Se crea el DTO dtoTipoEmpleo para representar la información de un tipo de empleo, incluyendo campos como 'id' y 'nombre', junto con sus métodos getter y setter.

---
+++ back_local/src/main/java/com/portfolioweb/aj/Dto/dtoTipoEmpleo.java
@@ -0,0 +1,31 @@
+package com.portfolioweb.aj.Dto;
+
+public class dtoTipoEmpleo {
+
+    private Long id;
+    private String nombre;
+
+    public dtoTipoEmpleo() {
+    }
+
+    public dtoTipoEmpleo(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
