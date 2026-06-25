107fbb77bac64dad526c95de04d73c64069e1fd7
ana.juarez <manitacoqui@gmail.com>
2026-06-25 10:50:31 -0300

    Se actualiza la clase Educacion para incluir nuevos campos como fechaInicio, fechaFin, y referencias a TipoEducacion y Organizacion, además de listas de habilidades y archivos adjuntos.

--- back_local/src/main/java/com/portfolioweb/aj/Entidad/Educacion.java
+++ back_local/src/main/java/com/portfolioweb/aj/Entidad/Educacion.java
@@ -1,52 +1,56 @@
-
 package com.portfolioweb.aj.Entidad;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
-@Entity 
+@Entity
 public class Educacion {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_educacion")
-    private int id; 
-    private String nombreE; 
+    private int id;
+
+    private String nombreE;
+
     private String descripcionE;
-    private String img;
-    private int anocomienzo;
-    
-    
-    //constructores
+
+    private String fechaInicio;
+
+    private String fechaFin;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_tipo_educacion")
+    private TipoEducacion tipoEducacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_organizacion")
+    private Organizacion organizacion;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "educacion_habilidades", joinColumns = @JoinColumn(name = "id_educacion"), inverseJoinColumns = @JoinColumn(name = "id_habilidades"))
+    private List<Habilidades> habilidades = new ArrayList<>();
+
+    @Lob
+    @Column(name = "archivo_imagen", nullable = true)
+    private byte[] archivoImagen;
+
+    @Lob
+    @Column(name = "archivo_pdf", nullable = true)
+    private byte[] archivoPdf;
+
     public Educacion() {
-    }
-
-    public Educacion(String nombreE, String descripcionE, String img, int anocomienzo) {
-        this.nombreE = nombreE;
-        this.descripcionE = descripcionE;
-        this.img = img;
-        this.anocomienzo = anocomienzo;     
-    
-    }
-    
-    //getters y setters
-    public String getImg() {    
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public int getAnocomienzo() {
-        return anocomienzo;
-    }
-
-    
-    public void setAnocomienzo(int anocomienzo) {
-        this.anocomienzo = anocomienzo;
     }
 
     public int getId() {
@@ -72,8 +76,60 @@ public class Educacion {
     public void setDescripcionE(String descripcionE) {
         this.descripcionE = descripcionE;
     }
-    
-    
-    
-    
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public TipoEducacion getTipoEducacion() {
+        return tipoEducacion;
+    }
+
+    public void setTipoEducacion(TipoEducacion tipoEducacion) {
+        this.tipoEducacion = tipoEducacion;
+    }
+
+    public Organizacion getOrganizacion() {
+        return organizacion;
+    }
+
+    public void setOrganizacion(Organizacion organizacion) {
+        this.organizacion = organizacion;
+    }
+
+    public List<Habilidades> getHabilidades() {
+        return habilidades;
+    }
+
+    public void setHabilidades(List<Habilidades> habilidades) {
+        this.habilidades = habilidades;
+    }
+
+    public byte[] getArchivoImagen() {
+        return archivoImagen;
+    }
+
+    public void setArchivoImagen(byte[] archivoImagen) {
+        this.archivoImagen = archivoImagen;
+    }
+
+    public byte[] getArchivoPdf() {
+        return archivoPdf;
+    }
+
+    public void setArchivoPdf(byte[] archivoPdf) {
+        this.archivoPdf = archivoPdf;
+    }
 }
