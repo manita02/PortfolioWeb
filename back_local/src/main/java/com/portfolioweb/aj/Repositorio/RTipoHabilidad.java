c1cb50a0ad3b78060d3aa01e2ae4d058c12c2a70
ana.juarez <manitacoqui@gmail.com>
2026-06-25 12:09:03 -0300

    Se añade la interfaz RTipoHabilidad para gestionar operaciones de persistencia de la entidad TipoHabilidad.

---
+++ back_local/src/main/java/com/portfolioweb/aj/Repositorio/RTipoHabilidad.java
@@ -0,0 +1,9 @@
+package com.portfolioweb.aj.Repositorio;
+
+import com.portfolioweb.aj.Entidad.TipoHabilidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RTipoHabilidad extends JpaRepository<TipoHabilidad, Long> {
+}
