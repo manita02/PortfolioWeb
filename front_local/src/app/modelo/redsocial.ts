078b2c5aa4d589fc313f2233ffc9a92b39682016
ana.juarez <manitacoqui@gmail.com>
2026-06-30 21:35:08 -0300

    Refactoriza la clase Redsocial a una interfaz, mejorando la definición de tipos y simplificando la estructura del modelo.

--- front_local/src/app/modelo/redsocial.ts
+++ front_local/src/app/modelo/redsocial.ts
@@ -1,12 +1,7 @@
-export class Redsocial {
-    id?: number;
-    nombreRedS: string;
-    img: string;
-    link: string;  
-    
-    constructor(nombreRedS: string, img: string, link: string) {
-        this.nombreRedS = nombreRedS;
-        this.img = img; 
-        this.link = link;
-    }
+
+export interface Redsocial {
+  id?: number;
+  nombreRedS: string;
+  img: string;
+  link: string;
 }
