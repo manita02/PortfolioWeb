95a26300d2054275a204bf6e57f233d0f3d185ea
ana.juarez <manitacoqui@gmail.com>
2026-07-17 15:20:52 -0300

    Reemplaza la clase Proyecto por un tipo exportado ProyectoDto, alineando la gestión de datos con el uso de DTOs en el proyecto.

--- front_local/src/app/modelo/proyecto.ts
+++ front_local/src/app/modelo/proyecto.ts
@@ -1,19 +1,2 @@
-export class Proyecto {
 
-    id?: number;
-    nombreE: string;
-    descripcionE: string;
-    link: string; 
-    img: string; 
-    anocomienzo: number;
-
-    constructor(nombreE: string, descripcionE: string, link: string, img: string, anocomienzo: number) {
-        this.nombreE = nombreE;
-        this.descripcionE = descripcionE;
-        this.link = link;
-        this.img = img; 
-        this.anocomienzo = anocomienzo; 
-    }
-
-
-}
+export type { ProyectoDto as Proyecto } from './proyecto.dto';
