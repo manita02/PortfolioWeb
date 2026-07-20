640ff5f9bdd469824ab7f7555c4ea9c195982578
ana.juarez <manitacoqui@gmail.com>
2026-07-20 16:31:00 -0300

    Refactoriza el componente Banner para utilizar un modal de edición. Se reemplaza el enlace de edición por un botón que abre el modal, y se añade la lógica para suscribirse a eventos de guardado. Se implementa la limpieza de suscripciones en el ciclo de vida del componente.

--- front_local/src/app/componentes/banner/banner.component.ts
+++ front_local/src/app/componentes/banner/banner.component.ts
@@ -1,25 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Banner } from 'src/app/modelo/banner';
+import { BannerModalService } from 'src/app/servicio/banner-modal.service';
 import { BannerService } from 'src/app/servicio/banner.service';
 import { TokenService } from 'src/app/servicio/token.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
 })
-export class BannerComponent implements OnInit {
+export class BannerComponent implements OnInit, OnDestroy {
 
   banner: Banner[] = []; 
+  isLogged = false;
 
-  constructor(private bannerS: BannerService, private tokenService: TokenService) { }
-  isLogged = false; 
+  private modalSavedSub?: Subscription;
+
+  constructor(
+    private bannerS: BannerService,
+    private tokenService: TokenService,
+    private bannerModal: BannerModalService
+  ) {}
+
   ngOnInit(): void {
     this.cargarBanner();
-    if(this.tokenService.getToken()){
-      this.isLogged = true; 
-    } else{
-      this.isLogged = false; 
-    }
+    this.isLogged = !!this.tokenService.getToken();
+    this.modalSavedSub = this.bannerModal.saved$.subscribe(() => {
+      this.cargarBanner();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.modalSavedSub?.unsubscribe();
+  }
+
+  openEdit(id: number): void {
+    this.bannerModal.openEdit(id);
   }
 
   cargarBanner(): void{
